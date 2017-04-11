97cb8f3119497ddc3b9085ae887d54393d0c9367
left thomas <leftthomas@qq.com>
2017-04-11 23:29:09 +0800

    网络结构设计测试初步

---
+++ src/Test.py
@@ -0,0 +1,4 @@
+from bias_neural_network import bias_neural_network
+
+image_shape = (32, 32, 3)
+network = bias_neural_network(image_shape=image_shape)
--- src/bias_neural_network.py
+++ src/bias_neural_network.py
@@ -1,7 +1,9 @@
 from keras.layers import Convolution2D, Flatten, Input, Dense
 
-image_shape = (32, 32, 3)
-input_rgb = Input(shape=image_shape, name='input_rgb')
-c1 = Convolution2D(filters=4, kernel_size=(2, 2), activation='relu')(input_rgb)
-d1 = Dense(units=64, activation='relu')(c1)
-f = Flatten()(d1)
+
+def bias_neural_network(image_shape):
+    input_rgb = Input(shape=image_shape, name='input_rgb')
+    c1 = Convolution2D(filters=4, kernel_size=(2, 2), activation='relu')(input_rgb)
+    d1 = Dense(units=64, activation='relu')(c1)
+    f = Flatten()(d1)
+    return f
