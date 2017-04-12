97c310d4f9f55f0cceea1081e8833e499f2c942f
left thomas <leftthomas@qq.com>
2017-04-12 15:30:35 +0800

    网络结构已设计完成

--- src/Test.py
+++ src/Test.py
@@ -1,4 +1,18 @@
+import numpy as np
+
 from bias_neural_network import BNN
 
-image_shape = (32, 32, 3)
-network = BNN(image_shape=image_shape)
+rgb_shape = (32, 32, 3)
+height_shape = (32, 32, 1)
+aux_shape = (32, 32, 1)
+
+rgb_inputs = np.array([1, 2, 3])
+height_inputs = np.array([1, 2, 3])
+aux_inputs = np.array([1, 2, 3])
+inputs = [rgb_inputs, height_inputs, aux_inputs]
+
+rgb_labes = np.array([1, 2, 3])
+height_labes = np.array([1, 2, 3])
+aux_labes = np.array([1, 2, 3])
+labes = [rgb_labes, height_labes, aux_labes]
+network = BNN(rgb_shape, height_shape, aux_shape, inputs, labes)
--- src/bias_neural_network.py
+++ src/bias_neural_network.py
@@ -1,9 +1,26 @@
-from keras.layers import Convolution2D, Flatten, Input, Dense
+from keras.layers import Input, Dense, concatenate
+from keras.models import Model
 
 
-def BNN(image_shape):
-    input_rgb = Input(shape=image_shape, name='input_rgb')
-    c1 = Convolution2D(filters=4, kernel_size=(2, 2), activation='relu')(input_rgb)
-    d1 = Dense(units=64, activation='relu')(c1)
-    f = Flatten()(d1)
-    return f
+def BNN(rgb_shape, height_shape, aux_shape, inputs, labes):
+    rgb_input = Input(rgb_shape)
+    rgb_dense = Dense(units=32, activation='relu')(rgb_input)
+    rgb_conv = Dense(units=16, activation='relu')(rgb_dense)
+    rgb_output = Dense(units=1, activation='sigmoid')(rgb_conv)
+
+    height_input = Input(height_shape)
+    merge_input = concatenate(inputs=[rgb_conv, height_input])
+    height_dense = Dense(units=32, activation='relu')(merge_input)
+    height_conv = Dense(units=16, activation='relu')(height_dense)
+    height_output = Dense(units=1, activation='sigmoid')(height_conv)
+
+    aux_input = Input(aux_shape)
+    merge_input = concatenate(inputs=[height_conv, aux_input])
+    aux_dense = Dense(units=32, activation='relu')(merge_input)
+    aux_conv = Dense(units=16, activation='relu')(aux_dense)
+    aux_output = Dense(units=1, activation='sigmoid')(aux_conv)
+
+    model = Model(inputs=[rgb_input, height_input, aux_input], outputs=[rgb_output, height_output, aux_output])
+    model.compile(optimizer='rmsprop', loss='binary_crossentropy', loss_weights=[0.3, 0.3, 0.4])
+    model.fit(inputs, labes, epochs=50, validation_split=0.2)
+    return model
